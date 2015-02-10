66761b343241d997b498e9a921b068848a498e2a
elihu <elihu.salcedo@gmail.com>
2015-02-10 12:42:16 +0100

    Añadida la estructura básica del proyecto libgdx

---
+++ v0_1/desktop/src/com/plearning/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.plearning.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.plearning.game.PLearningGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PLearningGame(), config);
+	}
+}
