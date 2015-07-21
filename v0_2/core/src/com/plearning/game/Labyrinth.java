6a296b165b360b130354e3c57573224357f75136
elihu <elihu.salcedo@gmail.com>
2015-07-21 12:46:31 +0200

    Some refactoring with Game class and GameScene class

---
+++ v0_2/core/src/com/plearning/game/Labyrinth.java
@@ -0,0 +1,64 @@
+package com.plearning.game;
+
+import java.util.Random;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+import com.badlogic.gdx.math.Vector2;
+
+public class Labyrinth {
+	//Free Blocks Determinist
+	private final Vector2 free1 = new Vector2(8, 5);
+	private final Vector2 free2 = new Vector2(6, 7);
+	private final Vector2 free3 = new Vector2(12, 9);
+	private final Vector2 free4 = new Vector2(5, 11);
+	private final Vector2 free5 = new Vector2(5, 13);
+	private final Vector2 free6 = new Vector2(10, 15);
+	private final Vector2 free7 = new Vector2(7, 17);
+	
+	private final int iIni = 5;
+	private final int iFin = 12;
+	private final int jIni = 5;
+	private final int jFin = 17;
+	private final Random rand = new Random();
+	PlearningGame game;
+	SpriteBatch batch;
+
+	
+	float scaleFactor = 35;
+	
+	AtlasRegion floor; //temporal hasta convertirlo en clase y comprobar overlaps
+
+	
+	public Labyrinth(PlearningGame plearning, AssetManager manager){
+		game = plearning;
+		batch = game.batch;
+		TextureAtlas atlas = manager.get("PLearning.pack", TextureAtlas.class);
+		floor = atlas.findRegion("floor-w1");
+	}
+	
+	public void draw(){
+		boolean sw=true;
+		for(int i = iIni; i<= iFin ; i++){
+			for(int j = jIni; j<= jFin; j+=2){
+				
+				if(i==free1.x && j== free1.y){}
+					else if(i==free2.x && j== free2.y){}
+						else if(i==free3.x && j== free3.y){}
+							else if(i==free4.x && j== free4.y){}
+								else if(i==free5.x && j== free5.y){}
+									else if(i==free6.x && j== free6.y){}
+										else if(i==free7.x && j== free7.y){}
+										else{
+											batch.begin();
+											batch.draw(floor, i*scaleFactor, j*scaleFactor);
+											batch.end();											
+										}
+				
+			}
+		}
+	}
+
+}
---
+++ v0_2/core/src/com/plearning/game/PlearningGameScene.java
@@ -0,0 +1,150 @@
+package com.plearning.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.FPSLogger;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class PlearningGameScene extends ScreenAdapter {
+	AssetManager manager;
+	
+	AtlasRegion background;
+	AtlasRegion control;
+	AtlasRegion ball;
+	AtlasRegion pause;
+	AtlasRegion options;
+	AtlasRegion play;
+	AtlasRegion restart;
+	AtlasRegion door;
+	
+	Vector3 touchPosition;
+	
+	SpriteBatch batch;
+	OrthographicCamera camera;
+	TextureAtlas atlas;
+	
+	Sound tapSound;
+	Music music;
+	
+	PlearningGame game;
+	Labyrinth labyrinth;
+	float scaleFactorX;
+	float scaleFactorY;
+	
+	static enum GameState{
+		INIT, CONTROLS, PLAYING, PAUSE, FINISH, OPTIONS
+	}
+	
+	GameState gameState = GameState.INIT;
+	
+	
+	
+	public PlearningGameScene (PlearningGame plearning) {
+		
+		game = plearning;
+		batch = game.batch;
+		camera = game.camera;
+		manager = game.manager;
+		atlas = game.atlas;
+		
+		scaleFactorX = game.scaleFactorX;
+		scaleFactorY = game.scaleFactorY;
+		
+		background = atlas.findRegion("background-w1");
+		control = atlas.findRegion("stopGreen");
+		ball = atlas.findRegion("blue");
+		pause = atlas.findRegion("pause");
+		options = atlas.findRegion("options");
+		play = atlas.findRegion("play");
+		restart = atlas.findRegion("restart");
+		door = atlas.findRegion("door");
+		
+		touchPosition = new Vector3();
+		
+		labyrinth = new Labyrinth (game, manager);
+		//Sound and Music
+		tapSound = manager.get("SOUNDS/pop.ogg", Sound.class);
+		music = manager.get("SOUNDS/music.mp3", Music.class);
+		music.setLooping(true);
+		music.play();
+		
+		
+		
+	}
+	private void updateScene(){
+		scaleFactorX = game.scaleFactorX;
+		scaleFactorY = game.scaleFactorY;
+		if(Gdx.input.justTouched()){
+			touchPosition.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPosition);
+			
+			tapSound.play();
+			System.out.println("Touched at: "+ gridValueX(touchPosition.x)+", "+gridValueY(touchPosition.y));
+		}
+	}
+	
+	private float gridValueX(float value){
+		return Math.round(value/scaleFactorX);
+	}
+	private float gridValueY(float value){
+		return Math.round(value/scaleFactorY);
+	}
+	
+	private void drawScene(){
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+		
+		batch.begin();
+
+		batch.disableBlending();
+		batch.draw(background, 0, 0);
+		batch.enableBlending();
+		batch.draw(door, 175, 630);
+		//batch.draw(labyrinth, 175, 140);
+		//batch.draw(labyrinth, 420, 630);
+		batch.draw(door, 420, 140);
+		batch.draw(control, 70, 175);
+		batch.draw(control, 70, 595);
+		batch.draw(ball, 175, 700);
+		batch.draw(ball, 245, 735);
+		batch.draw(ball, 350, 700);
+		batch.draw(ball, 420, 735);
+		batch.draw(options, 35, 35);
+		batch.draw(restart, 175, 35);
+		batch.draw(pause, 280, 35);
+		batch.draw(play, 395, 35);
+		
+		batch.end();
+		
+		labyrinth.draw();
+	}
+	private void resetScene(){
+		gameState = GameState.INIT;
+		
+	}
+
+	@Override
+	public void render (float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		updateScene();
+		drawScene();
+	}
+	
+	public void dispose(){
+		tapSound.dispose();
+		music.dispose();
+	}
+}
